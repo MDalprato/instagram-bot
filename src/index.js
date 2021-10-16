a3e7b2c6e012a73d3fedbec4eec56ced60f7f4f7
Marco Dalprato <marcodalprato@me.com>
2021-10-16 13:39:38 +0200

    Improved comments

--- src/index.js
+++ src/index.js
@@ -3,23 +3,30 @@ const { post } = require("request");
 require("tools-for-instagram");
 const moment = require('moment');
 
-let secondsBetweenChecks = 3600;
+let secondsBetweenChecks = 60;
 let previousPostTime;
+let username = 'chiaraferragni';
 
 (async () => {
 
-  console.log("\n -- Check and like chiara ferragni post --\n".bold.underline);
+  console.log("\n -- Check and like " + username + " post --\n".bold.underline);
   let ig = await login();
 
   await likeChiaraNewPost(ig);
   setInterval(await likeChiaraNewPost.bind(null, ig), 1000 * secondsBetweenChecks);
+
 })();
 
 async function likeChiaraNewPost(ig) {
 
-  console.log("Checking ...");
-  let posts = await getUserRecentPosts(ig, 'chiaraferragni');
+  console.log("likeChiaraNewPost ...");
+
+  let posts = await getUserRecentPosts(ig, username);
+
+
   if (posts != undefined || posts != null) {
+
+    console.log("Latest posts size: " + posts.length);
 
     let latestPost = posts[0];
     let latestPostTime = moment(latestPost.caption.created_at_utc);
@@ -37,14 +44,19 @@ async function likeChiaraNewPost(ig) {
       // post most updated than previous, like and comment it 
       await commentPost(ig, latestPost, "Wow 🔥");
       likePost(ig, latestPost);
-      console.log("Liking and commenting this post");
+      console.log("latestPostInfo:");
       console.log(latestPostInfo);
 
-    }else{
-      console.log("Nothing new from the user ...");
+      console.log("Like + comment latest post");
+
+
+    } else {
+      console.log("Nothing new from the user, skip");
     }
 
     previousPostTime = latestPostTime;
+    console.log("Updating time");
+
 
   }
   console.log("Finished checking posts, waiting " + secondsBetweenChecks + " seconds");
