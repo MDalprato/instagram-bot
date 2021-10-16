93c8b5d19eab7a398e8cf136aa1bd8908fe8d0f1
Marco Dalprato <marcodalprato@me.com>
2021-10-16 18:29:16 +0200

    Added procfile

--- src/likeChiaraNewPost.js
+++ src/likeChiaraNewPost.js
@@ -1,5 +1,4 @@
 const { post } = require("request");
-const http = require('http');
 require("tools-for-instagram");
 const moment = require('moment');
 
@@ -7,12 +6,6 @@ let secondsBetweenChecks = 60;
 let previousPostTime;
 let username = 'chiaraferragni';
 
-let app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');
-});
-
-app.listen(process.env.PORT , '127.0.0.1');
 console.log('Node server running on port ' + process.env.PORT );
 
 (async () => {
@@ -40,31 +33,20 @@ async function likeChiaraNewPost(ig) {
     let dateString = moment.unix(latestPost.caption.created_at_utc).format("MM/DD/YYYY HH:mm:ss");
 
     let latestPostInfo = {
-     // short_name: latestPost.location.short_name ? 'nothing' : latestPost.location.short_name ,
-     // name: latestPost.location.name  ? 'nothing' : latestPost.location.name ,
-      caption: latestPost.caption.text  ? 'nothing' : latestPost.caption.text ,
+      caption: latestPost.caption.text,
       created_at_utc: dateString  ,
     }
-
     if (latestPostTime.diff(previousPostTime) > 0 || previousPostTime == undefined) {
-
-      // post most updated than previous, like and comment it 
       await commentPost(ig, latestPost, "Wow 🔥");
       likePost(ig, latestPost);
       console.log("latestPostInfo:");
       console.log(latestPostInfo);
-
       console.log("Like + comment latest post");
-
-
     } else {
       console.log("Nothing new from the user, skip");
     }
-
     previousPostTime = latestPostTime;
     console.log("Updating time");
-
-
   }
   console.log("Finished checking posts, waiting " + secondsBetweenChecks + " seconds");
 
